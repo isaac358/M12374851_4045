3d9e7d7aa6eb1f6bb07ad40fa6b95fe207053548
Administrator <Administrator@DESKTOP-099UQRG>
2023-02-19 20:01:17 -0500

    Added comments to main class

--- src/main/java/com/plantplaces/plAntPlacesControler.java
+++ src/main/java/com/plantplaces/plAntPlacesControler.java
@@ -3,6 +3,11 @@ package com.plantplaces;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * 
+ * @author Administrator
+ * @main Class
+ */
 @Controller
 public class plAntPlacesControler {
 
